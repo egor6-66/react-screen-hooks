ad6a2c55ac098f7a0446d6eda05d774dce47cfff
Egor <egorraybul@gmail.com>
2024-07-15 11:43:45 +0300

    upd

--- src/bin/utils/debounce.ts
+++ src/bin/utils/debounce.ts
@@ -1,4 +1,7 @@
 function debounce(func: any, timeout = 300) {
+    if (typeof window === 'undefined') {
+        return () => '';
+    }
     let timer: any = null;
     return (...args: any) => {
         clearTimeout(timer);
